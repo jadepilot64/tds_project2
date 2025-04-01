c82c44ba4e3b2c95fd42c4012232358efa959228
Sharan R <sharans.33333@gmail.com>
2025-04-01 07:22:39 +0530

    Update

--- app/main.py
+++ app/main.py
@@ -205,4 +205,4 @@ async def root():
 # For local development
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=10000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
